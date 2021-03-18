1d2982674ab6aa1622198fbae8cf2080ee79648e
wender zacarias xavier <wender.xavier@thoughtworks.com>
2021-03-18 11:54:27 -0300

    add react-router lib to navigate between pages and configure routes

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+
+import './App.css';
+
+const HatsPage = () =>
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>;
+
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
