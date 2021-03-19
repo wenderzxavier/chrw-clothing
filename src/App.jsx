1b79befcce52de2f06c4b7a8c8e8c3c9c876fb9c
wender zacarias xavier <wender.xavier@thoughtworks.com>
2021-03-18 22:46:27 -0300

    add navigation bar with links to home, shop and contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
