7698536ba72617e1522812741aea3cf6dc54797b
wender zacarias xavier <wender.zacariasxavier@gmail.com>
2021-05-31 12:39:42 -0300

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51Hm46dG12ffOFJYqsHe5JRhyCrQp5LKNErMcvsqaoT749ypbSB0ceWOFkRAW27G5aS6KaHFPVHSDhdzs6AFEIUTQ00SK6Fe9Vr";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crwn Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
